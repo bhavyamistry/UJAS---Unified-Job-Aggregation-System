de159a6f154f02c1011b56026e42957b0d8dacaf
BHAVYA MISTRY <bhavya.mstry@gmail.com>
2024-04-15 00:13:17 -0400

    access keys removed

--- ats/settings.py
+++ ats/settings.py
@@ -163,17 +163,17 @@ DATABASES = {
     }
 }
 
-AWS_ACCESS_KEY_ID = 'AKIAURQZT2XIRIFUCAWW'
-AWS_SECRET_ACCESS_KEY = '3e8eZaJWgA8r010WxqZ//MBWnQb0EFy+bidigQvS'
-AWS_STORAGE_BUCKET_NAME = 'usersresumes'
-AWS_S3_REGION_NAME = 'us-east-2'  # e.g., 'us-east-1'
+AWS_ACCESS_KEY_ID = 'insert your Access Key'
+AWS_SECRET_ACCESS_KEY = 'insert your Secret Access Key'
+AWS_STORAGE_BUCKET_NAME = 'insert your S3 Bucket name'
+AWS_S3_REGION_NAME = 'insert your S3 Bucket Region Name'  # e.g., 'us-east-1'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_S3_USE_SSL = False  # You can set this to False to disable SSL (only for debugging)
 PUBLIC_RESUME_LOCATION = 'resumes'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_RESUME_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'ats.storage.s3_uri_storage.PublicResumeStorage'
+DEFAULT_FILE_STORAGE = 'insert your S3 Bucket domain or URL'
 AWS_S3_ENDPOINT_URL = f"https://s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 
 EMAIL_BACKEND = 'django_ses.SESBackend'
